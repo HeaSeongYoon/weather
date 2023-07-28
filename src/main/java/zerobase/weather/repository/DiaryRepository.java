4361fdb72bf12a62ce1b2ca174a7fa6f64626993
HeaSeongYoon <yoonhs989898@gmail.com>
2023-07-28 21:19:01 +0900

    MVC 패턴 -> DiaryController 완성

---
+++ src/main/java/zerobase/weather/repository/DiaryRepository.java
@@ -0,0 +1,8 @@
+package zerobase.weather.repository;
+
+import org.springframework.stereotype.Repository;
+
+//extends JpaMemoRepository<Diary, Integer>
+@Repository
+public class DiaryRepository {
+}
